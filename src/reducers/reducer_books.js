ff38ab481f669627bc32d9e254daf7d7360cc806
Jon Ramer <jramer87@gmail.com>
2017-09-06 13:55:30 -0500

    Sets up boilerplate and adds books_reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Object Oriented Javascript' },
+    { title: "You Don't Know JS: Scopes and Closures" },
+    { title: 'Test Driven Javascript Development' }
+  ]
+}
