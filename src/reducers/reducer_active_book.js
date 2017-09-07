04ba5cf8ef7decebf77ac201ac2eb4c719af4285
Jon Ramer <jramer87@gmail.com>
2017-09-07 14:08:55 -0500

    Adds view to show detail for the selected book

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,8 +1,8 @@
 // State argument references this reducers state not the entire app's state
 export default function(state = null, action) {
-  switch (action.type) {
+  switch(action.type) {
     case 'BOOK_SELECTED':
       return action.payload;
-  }
+   }
   return state;
 }
