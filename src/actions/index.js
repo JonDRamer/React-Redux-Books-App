0f91e8505f42334df91cdf5384c0ddcf16f3a2ee
Jon Ramer <jramer87@gmail.com>
2017-09-07 13:30:38 -0500

    Adds ActiveBook reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ export function selectBook(book) {
   // selectBook is an action creator and needs to return an action
   // which is just an object with a type property and a payload
   return {
-    type: 'BOOK_SELECTED',//Type is always uppercased
+    type: 'BOOK_SELECTED',//Type is always uppercased 
     payload: book
   };
 }
